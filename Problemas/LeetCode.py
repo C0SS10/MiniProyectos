c9b8180d96cc1468070ddd0502ea63d243ed9fca
Esteban Cossio <estebancogo47@gmail.com>
2023-01-26 20:57:37 -0500

    Aprendiendo a iterar matrices en python

--- Problemas/LeetCode.py
+++ Problemas/LeetCode.py
@@ -5,6 +5,18 @@ def runningSum(nums):
         nums[i] += nums[i-1]
     return nums
 
+def maximumWealth(accounts):
+    maxDinero = 0
+    for i in range(len(accounts)):
+        #   suma cada fila ya que representa el dinero de cada cliente
+        dineroActual = sum(accounts[i])
+        #   Si el dineroActual es mayor que el dinero máximo actual actualiza
+        #   si por ejemplo dinero actual es 16 y maxDinero es 78 no actualiza
+        maxDinero = max(dineroActual,maxDinero)
+    return maxDinero
+
 if __name__ == '__main__':
     nums = [1,2,3,4,5]
+    accounts = [[15,20,3],[3,2,8],[10,4,1]]
     print(runningSum(nums))
+    print(maximumWealth(accounts))
